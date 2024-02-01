f51e17a97b182a5a08749224bbf3bfba22a04d2c
spocky <nguemnangandrew@gmail.com>
2024-02-01 02:35:06 +0100

    add vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,6 +19,7 @@ export default function RootLayout({
       </head>
 
       {children}
+      <SpeedInsights />
     </html>
   );
 }
